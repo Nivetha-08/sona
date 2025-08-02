08ac4c97224dce7a40d32cfec97dae6c5b6b8324
Nivetha <nivethamaniofficial@gmail.com>
2025-08-02 09:44:21 +0530

    first commit

--- src/Components/Association.jsx
+++ src/Components/Association.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Association = () => {
   return (
     <>
-        <h1 className='text-success py-3 text-center'>Sona Groups Association with Eminent Personalities...</h1>
-        <img src="https://www.sonamedicalcollege.ac.in/wp-content/uploads/2022/09/sona-group.jpg" alt="association" />
+        <h1 className='text-success py-3 text-center  '>Sona Groups Association with Eminent Personalities...</h1>
+        <img src="https://www.sonamedicalcollege.ac.in/wp-content/uploads/2022/09/sona-group.jpg" alt="association" className='img-fluid' />
     </>
   )
 }
