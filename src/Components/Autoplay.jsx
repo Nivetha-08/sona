74747fa1217d084379c8ca1cb77aa0c726adc3bd
Nivetha <nivethamaniofficial@gmail.com>
2025-08-02 10:58:18 +0530

    first commit

--- src/Components/Autoplay.jsx
+++ src/Components/Autoplay.jsx
@@ -4,25 +4,19 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function AutoPlay() {
-  const settings = {
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 4,
-    autoplay: true,
-    speed: 10000,
-    autoplaySpeed: 1000,
-    cssEase: "linear"
-  };
-
+  
   return (
-    <div className="slider-container pt-2" >
-      <Slider {...settings}>
-        <div><span className="badge badge-pill bg-danger">new  </span><span className="text-success"> 4th Annual Day Celebration 2025</span></div>
-        <div><span className="badge badge-pill bg-danger">new  </span><span className="text-success"> International Day 2025</span></div>
-        <div><span className="badge badge-pill bg-danger">new  </span><span className="text-success"> The textile to chettiar Industrialist C. Valliappa</span></div>
-        <div><span className="badge badge-pill bg-danger">new  </span><span className="text-success"> Sona Accademy: Building the Future of Cricket</span></div>
-      </Slider>
-    </div>
+    <>
+      <div className="pt-3">
+        <marquee direction="left" scrollamount="5"><span className="badge badge-pill bg-danger">new</span><span className="text-success">  4th Annual Day Celebration 2025  </span>
+                                                  <span className="badge badge-pill bg-danger">new</span><span className="text-success">  International Day 2025  </span>
+                                                  <span className="badge badge-pill bg-danger">new</span><span className="text-success">  The textile to chettiar Industrialist C. Valliappa  </span>
+                                                  <span className="badge badge-pill bg-danger">new</span><span className="text-success">  Sona Accademy: Building the Future of Cricket  </span> 
+        </marquee>
+      
+      </div>
+    </>
+
   );
 }
 
--- src/Components/Facts.jsx
+++ src/Components/Facts.jsx
@@ -1,68 +1,84 @@
-import React from 'react'
 import { useEffect } from 'react'
-import { gsap } from 'gsap'
-import {ScrollTrigger} from 'gsap/ScrollTrigger'
+import  {gsap} from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+// import './Test.css'
 
 const Facts = () => {
 
-  useEffect(()=>{
+    useEffect(()=>{
 
-    gsap.registerPlugin(ScrollTrigger)
+      gsap.registerPlugin(ScrollTrigger)
 
-    gsap.to("#counter", {
-      innerText : 101000,
-      duration : 2,
-      snap:{innerText:1},
-      ease: "power1.out",
-      ScrollTrigger : {
-        trigger : "#counter",
-        start: "top center",
-        markers: true
-      }
-    });
+      gsap.to("#stu", {
+        innerText: '10100k',
+        duration: 2,
+        snap: { innerText: 1 }, 
+        ease: "power1.out",
+        scrollTrigger : {
+            trigger : '#showcase',
+            start: 'top 75%',
+        }
+      });
 
-    gsap.to("#counter", {
-      innerText : 500,
-      duration : 2,
-      snap:{innerText:1},
-      ease: "power1.out",
-      ScrollTrigger : {
-        trigger : "#counter",
-        start: "top center",
-        markers: true
-      }
-    });
+      gsap.to("#phd", {
+        innerText: 500,
+        duration: 2,
+        snap: { innerText: 1 }, 
+        ease: "power1.out",
+        scrollTrigger : {
+            trigger : '#showcase',
+            start: 'top 75%',
+        }
+      });
 
-    gsap.to("#counter", {
-      innerText : 100,
-      duration : 2,
-      snap:{innerText:1},
-      ease: "power1.out",
-      ScrollTrigger : {
-        trigger : "#counter",
-        start: "top center",
-        markers: true
-      }
-    });
+      gsap.to("#management", {
+        innerText: 100,
+        duration: 2,
+        snap: { innerText: 1 }, 
+        ease: "power1.out",
+        scrollTrigger : {
+            trigger : '#showcase',
+            start: 'top 75%',
+        }
+      });
 
-    gsap.to("#counter", {
-      innerText : 250,
-      duration : 2,
-      snap:{innerText:1},
-      ease: "power1.out",
-      ScrollTrigger : {
-        trigger : "#counter",
-        start: "top center",
-        markers: true
-      }
-    });
-  })
+      gsap.to("#awards", {
+        innerText: 250,
+        duration: 2,
+        snap: { innerText: 1 }, 
+        ease: "power1.out",
+        scrollTrigger : {
+            trigger : '#showcase',
+            start: 'top 75%',
+        }
+      });
+    }, [])
 
   return (
-    <>
-        <h2 className='text-center text-success'>Important Facts - Sona Institutions</h2>
-
-    </>
+    <div>
+        <div className='showcase row' id='showcase' style={{ background: '#1c3556' }}>
+          <div className="col-3 text-center">
+            <img src="https://www.sonamedicalcollege.ac.in/wp-content/uploads/2020/06/icon-trust.png" alt="" className='img-fluid mt-5' />
+            <div className='my-4 h1 text-white' id='stu'>ref1</div>
+            <div className='mb-5 h6 text-white'>STUDENTS</div>
+          </div>
+          <div className="col-3 text-center">
+            <img src="https://www.sonamedicalcollege.ac.in/wp-content/uploads/2020/06/icon-experts.png" alt="" className='img-fluid mt-5' />
+            <div className='my-4 h1 text-white' id='phd'>ref1</div>
+            <div className='mb-5 h6 text-white'>PHD</div>
+          </div>
+          <div className="col-3 text-center">
+            <img src="https://www.sonamedicalcollege.ac.in/wp-content/uploads/2020/06/icon-experience.png" alt="" className='img-fluid mt-5' />
+            <div className='my-4 h1 text-white' id='management'>ref1</div>
+            <div className='mb-5 h6 text-white'>TOP LEVEL MANAGEMENT EXP</div>
+          </div>
+          <div className="col-3 text-center">
+            <img src="https://www.sonamedicalcollege.ac.in/wp-content/uploads/2020/06/client.png" alt="" className='img-fluid mt-5' />
+            <div className='my-4 h1 text-white' id='awards'>ref1</div>
+            <div className='mb-5 h6 text-white'>AWARDS</div>
+          </div>
+        </div>
+    </div>
   )
 }
 
